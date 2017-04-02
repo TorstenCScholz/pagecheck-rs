3ee5841bb92f1b90f9f500c8d515b3692a314046
Torsten <doaschdn@googlemail.com>
2017-04-02 21:25:12 +0200

    Send email after checking webpage

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
+#![recursion_limit = "1024"]
+
 extern crate reqwest;
 extern crate select;
 extern crate clap;
+extern crate lettre;
+extern crate dotenv;
+
+#[macro_use]
+extern crate error_chain;
 
 use select::document::{Document};
 use select::predicate::{Class};
 
 use clap::{App, Arg};
 
+use lettre::email::EmailBuilder;
+use lettre::transport::EmailTransport;
+use lettre::transport::smtp::{SmtpTransportBuilder, SMTP_PORT};
+
 use std::io::Read;
 
-// fn print_usage(program: &str, opts: Options) {
-//     let brief = format!("Usage: {} FILE [options]", program);
-//     print!("{}", opts.usage(&brief));
-// }
+mod errors {
+    error_chain! { }
+}
+
+use errors::*;
+
+const EMAIL_USERNAME: &'static str = "EMAIL_USERNAME";
+const EMAIL_PASSWORD: &'static str = "EMAIL_PASSWORD";
+const EMAIL_TO: &'static str = "EMAIL_TO";
+const EMAIL_TO_FULLNAME: &'static str = "EMAIL_TO_FULLNAME";
+const EMAIL_FROM: &'static str = "EMAIL_FROM";
+const EMAIL_FROM_FULLNAME: &'static str = "EMAIL_FROM_FULLNAME";
+const EMAIL_SERVER_ADDRESS: &'static str = "EMAIL_SERVER_ADDRESS";
 
 // TODO: Use error type as return value
 fn get_webpage(url: &str) -> Document {
@@ -23,8 +43,10 @@ fn get_webpage(url: &str) -> Document {
     Document::from(&body[..])
 }
 
-fn main() {
+fn run() {
     // TODO: Crawl every X seconds with a maximum deviation of Y seconds
+
+    dotenv::dotenv().ok();
 
     let matches = App::new("pagecrawl")
                         .version("0.1")
@@ -47,14 +69,59 @@ fn main() {
 
     let document = get_webpage(&url[..]);
 
-    if let Some(quantitiy_field) = document.find(Class(class_name)).next() {
-        // println!("Field: {:?}", quantitiy_field);
+    if let Some(_) = document.find(Class(class_name)).next() {
         println!("Found!");
 
-        // TODO: Tweet about it
+        send_email("Product available. Buy now!", "The watched product seems to be available. Buy now before it is too late.");
     } else {
         println!("Not found!");
 
-        // TODO: Tweet about is
+        send_email("Product not available", "The watched product is not available.");
     }
+}
+
+fn send_email(subject: &str, content: &str) {
+    let email_username = std::env::var(EMAIL_USERNAME).unwrap();
+    let email_password = std::env::var(EMAIL_PASSWORD).unwrap();
+    let email_to = std::env::var(EMAIL_TO).unwrap();
+    let email_to_fullname = std::env::var(EMAIL_TO_FULLNAME).unwrap();
+    let email_from = std::env::var(EMAIL_FROM).unwrap();
+    let email_from_fullname = std::env::var(EMAIL_FROM_FULLNAME).unwrap();
+    let email_server_address = std::env::var(EMAIL_SERVER_ADDRESS).unwrap();
+
+    let email = EmailBuilder::new()
+        .to((email_to.as_str(), email_to_fullname.as_str()))
+        .from((email_from.as_str(), email_from_fullname.as_str()))
+        .subject(subject)
+        .text(content)
+        .build()
+        .unwrap();
+
+    let mut mailer = SmtpTransportBuilder::new((email_server_address.as_str(), SMTP_PORT))
+        .unwrap()
+        .credentials(email_username.as_str(), email_password.as_str())
+        .build();
+
+    let result = mailer.send(email);
+
+    if !result.is_ok() {
+        println!("Could not send email: {:?}", result);
+    }
+}
+
+fn main() {
+    run();
+    // if let Err(ref e) = run() {
+    //     println!("error: {}", e);
+    //
+    //     for e in e.iter().skip(1) {
+    //         println!("caused by: {}", e);
+    //     }
+    //
+    //     if let Some(backtrace) = e.backtrace() {
+    //         println!("backtrace: {:?}", backtrace);
+    //     }
+    //
+    //     std::process::exit(1);
+    // }
 }
